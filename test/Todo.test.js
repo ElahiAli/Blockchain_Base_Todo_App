25a0725df23994a3711ae19c46aa6d3365438f2f
ElahiAli <ali_elahi00@outlook.com>
2023-04-26 16:32:05 +0330

    functions : createTask, updateTask, getAllTask tested

--- test/Todo.test.js
+++ test/Todo.test.js
@@ -40,15 +40,109 @@ const { assert, expect } = require("chai");
           ).to.emit(todo, "Payed");
         });
 
-        it("users name should return his/her address", async () => {
+        it("users payment status should be true", async () => {
           await todo.initialPayment("LaraEmerson", { value: fee.toString() });
-          const userAddress = await todo.getUserAddress("LaraEmerson");
-          assert.equal(userAddress, deployer);
+          const payStatus = await todo.getUserPaymentStatus();
+          assert.equal(payStatus, true);
         });
       });
 
       describe("createTask", function () {
-        beforeEach(async function () {});
-        it("");
+        it("only owner can create new task", async () => {
+          await expect(
+            todo
+              .connect(client)
+              .createTask("Todo Unit Test", "writing test for Todo contract", 1)
+          ).to.be.reverted;
+        });
+
+        it("before creating the first task , payment needed", async () => {
+          await expect(
+            todo.createTask(
+              "Todo Unit Test",
+              "writing test for Todo contract",
+              1
+            )
+          ).to.be.reverted;
+        });
+
+        it("emit an event after creating a new task", async () => {
+          const fee = ethers.utils.parseEther("1");
+          await todo.initialPayment("LaraEmerson", { value: fee });
+          expect(
+            await todo.createTask(
+              "Todo Unit Test",
+              "writing test for Todo contract",
+              1
+            )
+          ).to.emit(todo, "TaskCreated");
+        });
+      });
+
+      describe("updateTask", function () {
+        beforeEach(async function () {
+          const fee = ethers.utils.parseEther("1");
+          await todo.initialPayment("LaraEmerson", { value: fee });
+          await todo.createTask(
+            "Todo Unit Test",
+            "writing test for Todo contract",
+            1
+          );
+        });
+
+        it("only owner can update a task", async () => {
+          await expect(todo.connect(client).updateTask(2, 1)).to.be.reverted;
+        });
+
+        it("updating the status of the task", async () => {
+          const initialTask = await todo.getTask(1);
+          const initialStatus = initialTask[2];
+
+          const updateTx = await todo.updateTask(2, 1);
+          await updateTx.wait(1);
+
+          const updatedTask = await todo.getTask(1);
+          const updatedStatus = updatedTask[2];
+
+          assert.equal(initialStatus.toString(), "1");
+          assert.equal(updatedStatus.toString(), "2");
+        });
+
+        it("emit an event after updating a task", async () => {
+          expect(await todo.updateTask(2, 1)).to.emit(todo, "TaskUpdated");
+        });
+      });
+
+      describe("getAllTasks", function () {
+        beforeEach(async function () {
+          const fee = ethers.utils.parseEther("1");
+          await todo.initialPayment("LaraEmerson", { value: fee });
+          await todo.createTask(
+            "Todo Unit Test",
+            "writing test for Todo contract",
+            1
+          );
+          await todo.createTask(
+            "Checking getAllTasks Function",
+            "this is just a normal test",
+            1
+          );
+        });
+
+        it("only owner can see all the tasks", async () => {
+          await expect(todo.connect(client).getAllTasks()).to.be.reverted;
+        });
+
+        it("getting all the tasks", async () => {
+          const taskList = await todo.getAllTasks();
+          const taskCount = await todo.getTaskCount();
+          let allTaskList = [];
+          for (let index = 0; index < taskCount; index++) {
+            allTaskList.push(taskList[index][0]);
+          }
+
+          assert.equal(allTaskList[0], "Todo Unit Test");
+          assert.equal(allTaskList[1], "Checking getAllTasks Function");
+        });
       });
     });
