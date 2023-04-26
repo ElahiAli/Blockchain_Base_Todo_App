2bab357e1bf44ced84a6b0e55293338390d16c5a
ElahiAli <ali_elahi00@outlook.com>
2023-04-26 11:24:55 +0330

    init hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    compilers: [{ version: "0.8.18" }],
+    compilers: [{ version: "0.8.18" }, { version: "0.8.8" }],
   },
   networks: {
     hardhat: {
